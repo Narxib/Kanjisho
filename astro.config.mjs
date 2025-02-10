e79ba7a4597b13be47e4e3b44ac8f5035898e50b
Brian Zaragoza Cerdá <brianzaragoza25@gmail.com>
2025-02-10 12:09:10 +0100

    improved data fetching and .env added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import tailwind from "@astrojs/tailwind";
 import react from '@astrojs/react';
@@ -7,5 +7,11 @@ import react from '@astrojs/react';
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
-  adapter: netlify()
-});
+  adapter: netlify(),
+  env: {
+    schema: {
+      API_KEY: envField.string({ context: "server", access: "secret" }),
+    }
+  }
+}
+);
