abbab61402edc0683aa5cdeeccca2d1be3c6e70d
Brian Zaragoza Cerdá <brianzaragoza25@gmail.com>
2025-02-18 10:50:30 +0100

    theme button added

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -1,18 +1,29 @@
-import moon from "../icons/moon.jsx"
-import sun from "../icons/sun.jsx"
-import { applyTheme } from "../utils/theme.js"
+import { useEffect, useState } from "react";
+import { applyTheme } from "../utils/theme.js";
+import MoonIcon from "../icons/moon.jsx";
+import SunIcon from "../icons/sun.jsx"; // Asegúrate de tener este icono
 
 export default function ThemeButton() {
-    let theme
+    const [theme, setTheme] = useState("")
+
+    useEffect(() => {
+        setTheme(localStorage.getItem("theme") || "light");
+    }, []);
+
     function toggleTheme() {
-        theme=localStorage.getItem("theme") || "light";
-        theme = theme === "dark" ? "light" : "dark";
-        applyTheme(theme);
+        const newTheme = theme === "dark" ? "light" : "dark";
+        applyTheme(newTheme);
+        setTheme(newTheme);
+        localStorage.setItem("theme", newTheme);
     }
 
     return (
-        <button class="flex absolute w-6 h-6 top-12 p-8 justify-center items-center hover:bg-black/20 right-12 rounded-full" onClick={()=>toggleTheme()} aria-label="Toggle theme">
-            <span>{theme === "dark" ? "🌞" : "🌙"}</span>
+        <button
+            className="flex text-red-400 absolute w-6 h-6 top-12 p-8 justify-center items-center hover:bg-black/20 right-12 rounded-full"
+            onClick={toggleTheme}
+            aria-label="Toggle theme"
+        >
+            {theme === "dark" ? "☀️" : "🌙"}
         </button>
-    )
+    );
 }
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,19 +1,19 @@
 // src/theme.ts
 export function applyTheme(theme) {
-   const nav = document.getElementById("navbar")
-    if (theme === "dark") {
-      document.documentElement.classList.add("dark");
-      nav.classList.add("dark")
-      localStorage.setItem("theme", "dark");
-    } else {
-      nav.classList.remove("dark")
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-    }
+  const nav = document.getElementById("navbar")
+  if (theme === "dark") {
+    document.documentElement.classList.add("dark");
+    nav.classList.add("dark")
+    localStorage.setItem("theme", "dark");
+  } else {
+    nav.classList.remove("dark")
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("theme", "light");
   }
-  
-  export function initTheme() {
-    const storedTheme = localStorage.getItem("theme") || "light";
-    applyTheme(storedTheme);
-  }
-  
+}
+
+export function initTheme() {
+  const storedTheme = localStorage.getItem("theme") || "light";
+  applyTheme(storedTheme);
+}
+
