8c810f4deb7085a18d8c8547e80085be8352cf98
Brian <brianzaragoza25@gmail.com>
2025-03-17 12:16:15 +0100

    Improved kanji display

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -1,29 +1,29 @@
 import { useEffect, useState } from "react";
 import { applyTheme } from "../utils/theme.js";
-import MoonIcon from "../icons/moon.jsx";
-import SunIcon from "../icons/sun.jsx"; // Asegúrate de tener este icono
+import { MoonIcon } from "../icons/moon.jsx";
+import { SunIcon } from "../icons/sun";
 
 export default function ThemeButton() {
-    const [theme, setTheme] = useState("")
+  const [theme, setTheme] = useState("");
 
-    useEffect(() => {
-        setTheme(localStorage.getItem("theme") || "light");
-    }, []);
+  useEffect(() => {
+    setTheme(localStorage.getItem("theme") || "light");
+  }, []);
 
-    function toggleTheme() {
-        const newTheme = theme === "dark" ? "light" : "dark";
-        applyTheme(newTheme);
-        setTheme(newTheme);
-        localStorage.setItem("theme", newTheme);
-    }
+  function toggleTheme() {
+    const newTheme = theme === "dark" ? "light" : "dark";
+    applyTheme(newTheme);
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+  }
 
-    return (
-        <button
-            className="flex text-red-400 bg-white/20 w-6 h-6  p-8 justify-center items-center hover:bg-black/20 "
-            onClick={toggleTheme}
-            aria-label="Toggle theme"
-        >
-            {theme === "dark" ? "☀️" : "🌙"}
-        </button>
-    );
+  return (
+    <button
+      className="flex w-16 h-16 justify-center items-center rounded-bl-lg hover:bg-gray-400/20 "
+      onClick={toggleTheme}
+      aria-label="Toggle theme"
+    >
+      {theme == "dark" ? <SunIcon /> : <MoonIcon />}
+    </button>
+  );
 }
